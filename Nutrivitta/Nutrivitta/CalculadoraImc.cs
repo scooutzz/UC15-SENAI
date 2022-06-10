1c9cdcbefa8ec3816ceb202e4793bd7c8d66d28a
Fabio Lazaro de Almeida <fabioalmeida2004@gmail.com>
2022-06-10 17:06:48 -0300

    Desenho do teste calculo IMC

---
+++ Nutrivitta/Nutrivitta/CalculadoraImc.cs
@@ -0,0 +1,43 @@
+﻿namespace Nutrivitta
+{
+    public static class CalculadoraImc
+    {
+        public static string ClassificacaoIMC(double peso, double altura)
+        {
+            if (peso <= 0 || altura <= 0)
+            {
+                return "Peso e/ou altura não podem ser negativos ou ter valores iguais a zero(0).";
+            }
+
+            double imc = peso / (altura * altura);
+            string classificacao = "";
+
+            if (imc < 18.5)
+            {
+                classificacao = "Abaixo do peso";
+            }
+            else if (imc < 25)
+            {
+                classificacao = "Peso normal";
+            }
+            else if (imc < 30)
+            {
+                classificacao = "Sobrepeso";
+            }
+            else if (imc < 40)
+            {
+                classificacao = "Obesidade grau I";
+            }
+            else if (imc < 40)
+            {
+                classificacao = "Obesidade grau II";
+            }
+            else if (imc >= 40)
+            {
+                classificacao = "Obesidade grau III";
+            }
+
+            return "IMC: {imc:0.00} kg/m²\n{classificacao}";
+        }
+    }
+}
---
+++ Nutrivitta/TestXUnitIMC/UnitTest1.cs
@@ -0,0 +1,40 @@
+using Nutrivitta;
+using System;
+using Xunit;
+
+namespace TestXUnitIMC
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CalcularIMC()
+        {
+            // Arrange
+            double peso = 0;
+            double altura = 0;
+            string resultadoEsperado = "Peso e/ou altura não podem ser negativos ou ter valores iguais a zero(0).";
+
+            // Act
+            var resultadoObtido = CalculadoraImc.ClassificacaoIMC(peso, altura);
+
+            // Asssert
+            Assert.Equal(resultadoEsperado, resultadoObtido);
+        }
+
+        [Theory]
+        [InlineData(-5, -5, "Peso e/ou altura não podem ser negativos ou ter valores iguais a zero(0).")]
+        [InlineData(70, 1.75, "IMC: 22,86 kgm²\nPeso normal")]
+        [InlineData(63, 1.85, "IMC: 18,41 kgm²\nAbaixo do peso")]
+        [InlineData(78, 1.65, "IMC: 28,65 kg/m²\nSobrepeso")]
+        [InlineData(60, 1.40, "IMC: 30,61 kg/m²\nObesidade grau I")]
+        [InlineData(90, 1.60, "IMC: 35,16 kg/m²\nObesidade grau II")]
+        [InlineData(140, 1.80, "IMC: 43,21 kg/m²\nObesidade grau III")]
+
+        public void CalcularImcLista(double peso, double altura, string resultadoEsperado)
+        {
+            var resultadoObtido = CalculadoraImc.ClassificacaoIMC(peso, altura);
+
+            Assert.Equal(resultadoEsperado, resultadoEsperado);
+        }
+    }
+}
